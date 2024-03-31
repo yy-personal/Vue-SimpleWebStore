72fbbc6dabdf213e3305cf46b75a049ec2f35502
Haider Zaidi <contact@haiderzaidi.ca>
2024-04-01 03:06:15 +0800

    Setup back-end server to function with lambda + ddb

---
+++ checkout/server/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.c74sciiigls1.ap-southeast-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'password1',
+    DB_DATABASE : 'webappdb'
+});
